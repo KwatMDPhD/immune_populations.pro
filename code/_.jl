b8684124acf6273cddd7b3df7adc6ae13f5939da
KwatMDPhD <kwat.me@icloud.com>
2022-02-19 14:20:00 -0700

    :)

--- code/_.jl
+++ code/_.jl
@@ -1,19 +1,18 @@
-# ---
-# Default lean project things
+# ----------------------------------------------------------------------------------------------- #
+# Default lean-project things
 
-#using LeanProject
+using LeanProject
 
 se = joinpath(dirname(@__DIR__), "input", "setting.json")
 
-#PAR, PAI, PAC, PAO = LeanProject.get_project_path(se)
+PAR, PAI, PAC, PAO = LeanProject.get_project_path(se)
 
-#SE = LeanProject.read_setting(se)
+SE = LeanProject.read_setting(se)
 
-# ---
-# Project specific things
-
-using PyCall
+# ----------------------------------------------------------------------------------------------- #
+# Project-specific things
 
 using OnePiece
+using PyCall
 
 kwat = pyimport("kwat")
